7cc7603208f7afda2014b49845328dc8cc2d9554
Sam Deane <sam@elegantchaos.com>
2024-10-31 12:24:42 +0000

    put back SignalSupport to minimise changes

--- Sources/SwiftGodot/Core/GenericSignal.swift
+++ Sources/SwiftGodot/Core/GenericSignal.swift
@@ -67,50 +67,6 @@ public class GenericSignal<each T: VariantStorable> {
 
 }
 
-/// This is a convenience object used as a helper for signals, all it does is
-/// register a method with godot called `proxy` that will invoke
-/// the callback defined in the public `proxy` variable here.
-///
-/// Usage:
-/// ```
-/// let demo = SignalProxy ()
-/// demo.proxy = { print ("called back") }
-///
-/// // Let godot call `demo` and its proxy method.
-/// invokeScript ("myDemo.proxy ()", params: ["myDemo", demo])
-/// ```
-public class SignalProxy: Object {
-    public static var proxyName = StringName("proxy")
-    static var initClass: Bool = {
-        register(type: SignalProxy.self)
-
-        let s = ClassInfo<SignalProxy>(name: "SignalProxy")
-
-        s.registerMethod(name: SignalProxy.proxyName, flags: .default, returnValue: nil, arguments: [], function: SignalProxy.proxyFunc)
-        return true
-    }()
-
-    /// The code invoked when Godot invokes the `proxy` method on this object.
-    public typealias Proxy = (borrowing Arguments) -> Void
-    public var proxy: Proxy?
-
-    public required init() {
-        let _ = SignalProxy.initClass
-        super.init()
-    }
-
-    public required init(nativeHandle: UnsafeRawPointer) {
-        super.init(nativeHandle: nativeHandle)
-    }
-
-    func proxyFunc(args: borrowing Arguments) -> Variant? {
-        proxy?(args)
-        return nil
-    }
-}
-
-/// The simple signal is used to raise signals that take no arguments and return no values.
-public typealias SimpleSignal = GenericSignal< /* no generic arguments */>
 
 extension Arguments {
     enum UnpackError: Error {
