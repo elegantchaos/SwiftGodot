13d4b3af9b46b04c4704bfdb30e21800804e588d
Sam Deane <sam@elegantchaos.com>
2025-01-27 10:23:38 +0000

    cleaned up

--- Sources/SwiftGodot/ExtensionInterface.swift
+++ Sources/SwiftGodot/ExtensionInterface.swift
@@ -16,41 +16,6 @@ public protocol ExtensionInterface {
 
 }
 
-// public class OpaqueExtensionInterface: ExtensionInterface {
-
-//     /// If your application is crashing due to the Variant leak fixes, please
-//     /// enable this flag, and provide me with a test case, so I can find that
-//     /// pesky scenario.
-//     public let experimentalDisableVariantUnref = false
-
-//     private let library: OpaquePointer
-//     private let getProcAddrFun: OpaquePointer
-
-//     public init(library: OpaquePointer, getProcAddrFun: OpaquePointer) {
-//         self.library = library
-//         self.getProcAddrFun = getProcAddrFun
-//     }
-
-//     public func variantShouldDeinit(content: UnsafeRawPointer) -> Bool {
-//         return !experimentalDisableVariantUnref
-//     }
-
-//     public func objectShouldDeinit(handle: UnsafeRawPointer) -> Bool {
-//         return true
-//     }
-
-//     public func objectInited(object: Wrapped) {}
-
-//     public func objectDeinited(object: Wrapped) {}
-
-//     public func getLibrary() -> UnsafeMutableRawPointer {
-//         return UnsafeMutableRawPointer(mutating: library)
-//     }
-
-//     public func getProcAddr() -> OpaquePointer { getProcAddrFun }
-
-// }
-
 class LibGodotExtensionInterface: ExtensionInterface {
 
     /// If your application is crashing due to the Variant leak fixes, please
